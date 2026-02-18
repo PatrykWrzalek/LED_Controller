2bc8041cd6754fc43208c7ac2f76ca3a065ce03c
PatrykWrzalek <wrzalek.patryk@gmail.com>
2026-02-18 19:41:07 +0100

    Test example for status_led

---
+++ components/drv_ledc/include/drv_ledc.h
@@ -0,0 +1,8 @@
+
+#ifndef DRV_LEDC
+#define DRV_LEDC
+
+void example_ledc_init(void);
+void example_ledc(void);
+
+#endif
--- main/main.c
+++ main/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include "drv_ledc.h"
 
 void app_main(void)
 {
-
+    example_ledc();
 }
